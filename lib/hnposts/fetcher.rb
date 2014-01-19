7a49d199f13ffaffa85829484cfd9e63f629693f
Sankaranarayanan Viswanathan <rationalrevolt@gmail.com>
2014-01-18 23:43:26 -0500

    Consider posts that don't have a vote arrow, points or comments, or that do not point to an external URL

--- lib/hnposts/fetcher.rb
+++ lib/hnposts/fetcher.rb
@@ -4,59 +4,63 @@ module HNPosts
 
     HN_URL = "https://news.ycombinator.com/"
 
-    def initialize
-      @hn_doc = Nokogiri::HTML(_contents)
-    end
-
     def fetch_posts
-      return @posts if @posts
-      
-      post_ids, titles, urls, points, comments = _post_ids, _titles, _urls, _points, _comments
-      post_count = post_ids.size
-      @posts = (0...post_count).collect do | indx |
-        Post.new post_id: post_ids[indx],
-                 title: titles[indx],
-                 url: urls[indx],
-                 points: points[indx],
-                 comments: comments[indx],
-                 position: (indx + 1)
+      posts = []
+      post_nodes = Nokogiri::HTML(contents).css('td[class=title]:nth-of-type(3)').map {|n| n.parent}
+
+      post_nodes.each_with_index do | node, indx |
+        posts << Post.new({
+                           post_id: (post_id node),
+                           title: (title node),
+                           url: (url node),
+                           points: (points node),
+                           comments: (comments node),
+                           position: (indx + 1)
+                         })
       end
+
+      posts
     end
       
     private
 
-    attr_reader :hn_doc
-
-    def _post_ids
-      hn_doc.css('td > center > a').collect { |tag| /up_(\d+)/.match(tag.attributes['id'].value)[1] }
-    end
-    
-    def _titles
-      hn_doc.css('td[class=title] a').collect { |tag| tag.text.strip }
-    end
-
-    def _urls
-      hn_doc.css('td[class=title] a').collect { |tag| tag.attributes['href'].value }
-    end
-
-    def _points
-      hn_doc.css('td[class=subtext] span').collect { |tag| tag.text[/\d+/].to_i }
-    end
-
-    def _comments
-      cntr = 0
-      hn_doc.css('td[class=subtext] a').reduce([]) do | acc, tag |
-        acc.tap do |acc|
-          acc << tag.text[/\d+/].to_i if cntr.odd?
-          cntr = cntr.next
-        end
+    def post_id tr
+      vote_node = tr.css('td > center > a')[0]
+      title_node = tr.css('td[class=title] > a')[0]
+      if vote_node
+        /^up_(\d+)$/.match(vote_node.attributes['id'])[1]
+      else
+        /^item\?id=(\d+)$/.match(title_node.attributes['href'])[1]
       end
     end
     
-    def _contents
-      open(HN_URL)
+    def title tr
+      tr.css('td[class=title] > a')[0].text.strip
+    end
+
+    def url tr
+      href = tr.css('td[class=title] > a')[0].attributes['href'].value
+      /^item\?id=\d+$/ =~ href ? HN_URL + href : href
+    end
+
+    def points tr
+      result = tr.next_sibling.css('span')[0]
+      result &&= /^(\d+) points$/.match(result.text)
+      result &&= result[1]
+      result.to_i
+    end
+
+    def comments tr
+      result = tr.next_sibling.css('a')[1]
+      result &&= /^(\d+) comments$/.match(result.text)
+      result &&= result[1]
+      result.to_i
     end
     
+    def contents
+      open(HN_URL)
+    end
+
   end
 
 end
